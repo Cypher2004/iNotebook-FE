f364da282dffb819f44a9371237331a23046e4e5
Cypher2004 <truepratham2801@gmail.com>
2024-07-23 14:41:31 +0530

    Changes in build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import {disableReactDevTools} from '@fvilers/disable-react-devtools'; 
-
+const root = document.getElementById('root');
 root.render(
   <React.StrictMode>
     <App />
